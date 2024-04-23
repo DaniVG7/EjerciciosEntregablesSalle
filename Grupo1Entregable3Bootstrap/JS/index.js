671a97474c3c3672911b00682bde0503c004020c
Daniel Vargas García <161933152+DaniVG7@users.noreply.github.com>
2024-04-23 20:34:49 +0200

    highlights

--- Grupo1Entregable3Bootstrap/JS/index.js
+++ Grupo1Entregable3Bootstrap/JS/index.js
@@ -1,10 +1,10 @@
-function printAllDivs(){
-    console.log(document.querySelectorAll("div"))
-    }
-    window.addEventListener("load", printAllDivs);
-    document.addEventListener("mouseover", function (evento) {
-      evento.target.classList.add("blackBackground")
-    })
-    document.addEventListener("mouseout", function (evento) {
-      evento.target.classList.remove("blackBackground")
-    })
+// function printAllDivs(){
+//     console.log(document.querySelectorAll("div"))
+//     }
+//     window.addEventListener("load", printAllDivs);
+//     document.addEventListener("mouseover", function (evento) {
+//       evento.target.classList.add("blackBackground")
+//     })
+//     document.addEventListener("mouseout", function (evento) {
+//       evento.target.classList.remove("blackBackground")
+//     })
